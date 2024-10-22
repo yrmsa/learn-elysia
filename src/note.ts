5f136ae78def1363544c920e8108dc79b984c316
yrmsa <yrmsa>
2024-10-22 21:48:12 +0700

    feat: add add, remove, update function

--- src/note.ts
+++ src/note.ts
@@ -2,11 +2,30 @@ import { Elysia, error, t } from "elysia";
 
 class Note {
 	constructor(public data: string[] = ["Moonhalo"]) {}
+
+	add(note: string) {
+		this.data.push(note);
+		return this.data;
+	}
+
+	remove(index: number) {
+		return this.data.splice(index, 1);
+	}
+
+	update(index: number, note: string) {
+		this.data[index] = note;
+		return this.data[index];
+	}
 }
 
 export const note = new Elysia()
 	.decorate("note", new Note())
 	.get("/note", ({ note }) => note.data)
+	.put("/note", ({ note, body: { data } }) => note.add(data), {
+		body: t.Object({
+			data: t.String(),
+		}),
+	})
 	.get(
 		"/note/:index",
 		({ note, params: { index } }) => {
@@ -15,6 +34,33 @@ export const note = new Elysia()
 		{
 			params: t.Object({
 				index: t.Number(),
+			}),
+		},
+	)
+	.delete(
+		"/note/:index",
+		({ note, params: { index } }) => {
+			if (index in note.data) return note.remove(index);
+			return error(422);
+		},
+		{
+			params: t.Object({
+				index: t.Number(),
+			}),
+		},
+	)
+	.patch(
+		"/note/:index",
+		({ note, params: { index }, body: { data }, error }) => {
+			if (index in note.data) return note.update(index, data);
+			return error(422);
+		},
+		{
+			params: t.Object({
+				index: t.Number(),
+			}),
+			body: t.Object({
+				data: t.String(),
 			}),
 		},
 	);
